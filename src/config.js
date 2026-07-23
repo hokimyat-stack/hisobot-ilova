b2a9a9d74ec7fb606ccc99a2d52fe326b4e8e5b9
hokimyat-stack <hokimyatgps@gmail.com>
2026-07-23 15:43:03 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,8 @@
 // src/config.js
-export const API_URL = 'https://script.google.com/macros/s/AKfycbxyIZQObWouqge3qNZs00w3S3fi1RYVnh8ijQTO-t-QIkLqJA7ip-P2ksW3nhj1CI-0/exec';
+export const API_URL = 'https://script.google.com/macros/s/AKfycbyIRVGLf-9nnOiBLPFqbXDeqL3XuI7pejWW5NBD413gsAnoUOVhrGcz2CvU0yO7liQ4/exec';
 export const KALIT = '7Kf92mQ';
-
 export const GITHUB_RELEASES = 'https://api.github.com/repos/hokimyat-stack/hisobot-ilova/releases/latest';
 export const ILOVA_VERSIYA = '3.0.0';
-
 export const RANG = {
   asosiy: '#1758B8', toq: '#17212B', fon: '#F6F8FB', oq: '#FFFFFF',
   yashil: '#1E9E56', sariq: '#E8A013', qizil: '#D33B2F', kul: '#8A97A6', chiziq: '#E3E9F1'
